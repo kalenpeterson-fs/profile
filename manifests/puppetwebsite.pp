f1e21fd3f3cf8a7461c3c1664187446405f3cb3c
Kalen Peterson <kpeterson@forsythe.com>
2016-07-26 11:28:35 -0500

    Working Profile

---
+++ manifests/puppetwebsite.pp
@@ -0,0 +1,31 @@
+# == Class: profile::puppetwebsite
+#
+class profile::puppetwebsite (
+  $web_root   = '/srv/puppet',
+  $port       = 8000,
+  $web_source = 'https://github.com/puppetlabs/exercise-webpage.git'
+){
+
+  # Ensure directory tree exists
+  exec { 'mkdir_web_root':
+    command => "mkdir -p ${web_root}",
+    creates => $web_root,
+  }
+
+  # Pull latest web source from github
+  vcsrepo { $web_root:
+    ensure   => latest,
+    provider => git,
+    source   => $web_source,
+    require  => Exec['mkdir_web_root'],
+  }
+
+  # Setup nginx
+  class { '::nginx': }
+
+  # Create a server definition for this website
+  ::nginx::server { 'puppet':
+    port     => $port,
+    web_root => $web_root,
+  }
+}
